49253db67094c1ab9eb218eba639f3e80c04a8ac
saroar <saroar9@gmail.com>
2017-09-25 20:57:11 +0300

    update5

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -7,17 +7,7 @@ class NotifyMailer < ApplicationMailer
       to: "lemiszewski@gmx.com",
       subject: "Wow my gem is working Awesome Job Saroar",
       :html_body => '<h1>Hello there!</h1>',
-      :body => "In case you can't read html, Hello there.",
-      :via => :smtp,
-      :via_options => {
-          :address => 'smtp.gmail.com',
-          :port => '587',
-          :domain => 'gmail.com',
-          user_name: ENV['GMAIL_USER'],
-          password: ENV['GMAIL_PASS'],
-          :authentication => :plain,
-          :enable_starttls_auto => true
-      }
+      :body => "In case you can't read html, Hello there."
       )
   end
 end
