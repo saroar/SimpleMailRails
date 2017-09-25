eaf6254b18094ab86d2ffbf25511d66a823fc910
saroar <saroar9@gmail.com>
2017-09-25 21:37:33 +0300

    update 9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,11 +92,11 @@ Rails.application.configure do
   SimpleMail.options = {
     :via => :smtp,
     :via_options => {
-        :address => 'smtp.gmail.com',
+        :address => 'smtp.sendgrid.net',
         :port => '587',
         :domain => 'gmail.com',
-        user_name: ENV['GMAIL_USER'],
-        password: ENV['GMAIL_PASS'],
+        user_name: ENV['SENDGRID_USERNAME'],
+        password: ENV['SENDGRID_PASSWORD'],
         :authentication => :plain,
         :enable_starttls_auto => true
     }
