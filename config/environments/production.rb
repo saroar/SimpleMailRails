a1aa5042799bb0ef38990b0572c572a22bef2598
saroar <saroar9@gmail.com>
2017-09-25 21:04:20 +0300

    update6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,11 @@ Rails.application.configure do
   #     :authentication => "plain",
   #     :enable_starttls_auto => true
   # }
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 end
